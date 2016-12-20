617b90ef3d2b1d46e7b306fa26a504c04130a7b7
Dimitrios Kalaitzidis <kalaitzidis34@gmail.com>
2016-12-20 22:52:58 +0200

    Revoved HEX

--- UIColorExtension.swift
+++ UIColorExtension.swift
@@ -2,227 +2,201 @@
 //  UIColorExtension.swift
 //  UIColorExtension
 //
-//  Created by Dimitrios Kalaitzidis on 29/05/16.
+//  Created by Dimitrios Kalaitzidis on 20/12/16.
 //  Copyright © 2016 Dimitrios Kalaitzidis. All rights reserved.
 //
 
 import UIKit
 
-extension UIColor{
+extension UIColor {
     
-    convenience init(R: Int, G: Int, B: Int){
-        self.init(red: CGFloat(R) / 255, green: CGFloat(G) / 255, blue: CGFloat(B) / 255, alpha: 1.0)
+    convenience init(Red: Int, Green: Int, Blue: Int) {
+        self.init(red: CGFloat(Red) / 255, green: CGFloat(Red) / 255, blue: CGFloat(Red) / 255, alpha: 1.0)
     }
-    
     
     //FlatUIColors - Values taken from flatuicolors.com
-    class func turquoise() -> UIColor{
-        return UIColor(R: 26, G: 188, B: 156)
+    class func turquoise() -> UIColor {
+        return UIColor(Red: 26, Green: 188, Blue: 156)
     }
     
-    class func emerald() -> UIColor{
-        return UIColor(R: 46, G: 204, B: 113)
+    class func emerald() -> UIColor {
+        return UIColor(Red: 46, Green: 204, Blue: 113)
     }
     
-    class func peterRiver() -> UIColor{
-        return UIColor(R: 52, G: 152, B: 219)
+    class func peterRiver() -> UIColor {
+        return UIColor(Red: 52, Green: 152, Blue: 219)
     }
     
-    class func amethyst() -> UIColor{
-        return UIColor(R: 155, G: 89, B: 182)
+    class func amethyst() -> UIColor {
+        return UIColor(Red: 155, Green: 89, Blue: 182)
     }
     
-    class func wetAsphalt() -> UIColor{
-        return UIColor(R: 52, G: 73, B: 94)
+    class func wetAsphalt() -> UIColor {
+        return UIColor(Red: 52, Green: 73, Blue: 94)
     }
     
-    class func greenSea() -> UIColor{
-        return UIColor(R: 22, G: 160, B: 133)
+    class func greenSea() -> UIColor {
+        return UIColor(Red: 22, Green: 160, Blue: 133)
     }
     
-    class func nephritis() -> UIColor{
-        return UIColor(R: 39, G: 174, B: 96)
-    }
-
-    class func belizeHole() -> UIColor{
-        return UIColor(R: 41, G: 128, B: 185)
+    class func nephritis() -> UIColor {
+        return UIColor(Red: 39, Green: 174, Blue: 96)
     }
     
-    class func wisteria() -> UIColor{
-        return UIColor(R: 142, G: 68, B: 173)
-    }
-
-    class func midnightBlue() -> UIColor{
-        return UIColor(R: 44, G: 62, B: 80)
+    class func belizeHole() -> UIColor {
+        return UIColor(Red: 41, Green: 128, Blue: 185)
     }
     
-    class func sunFlower() -> UIColor{
-        return UIColor(R: 241, G: 196, B: 15)
+    class func wisteria() -> UIColor {
+        return UIColor(Red: 142, Green: 68, Blue: 173)
     }
     
-    class func carrot() -> UIColor{
-        return UIColor(R: 230, G: 126, B: 34)
-    }
-
-    class func alizarin() -> UIColor{
-        return UIColor(R: 231, G: 76, B: 60)
+    class func midnightBlue() -> UIColor {
+        return UIColor(Red: 44, Green: 62, Blue: 80)
     }
     
-    class func clouds() -> UIColor{
-        return UIColor(R: 236, G: 240, B: 241)
-    }
-
-    class func concrete() -> UIColor{
-        return UIColor(R: 149, G: 165, B: 166)
+    class func sunFlower() -> UIColor {
+        return UIColor(Red: 241, Green: 196, Blue: 15)
     }
     
-    class func orange() -> UIColor{
-        return UIColor(R: 243, G: 156, B: 18)
+    class func carrot() -> UIColor {
+        return UIColor(Red: 230, Green: 126, Blue: 34)
     }
-
-    class func pumpkin() -> UIColor{
-        return UIColor(R: 211, G: 84, B: 0)
+    
+    class func alizarin() -> UIColor {
+        return UIColor(Red: 231, Green: 76, Blue: 60)
     }
-
-    class func pomegranate() -> UIColor{
-        return UIColor(R: 192, G: 57, B: 43)
+    
+    class func clouds() -> UIColor {
+        return UIColor(Red: 236, Green: 240, Blue: 241)
     }
-
-    class func silver() -> UIColor{
-        return UIColor(R: 189, G: 195, B: 199)
+    
+    class func concrete() -> UIColor {
+        return UIColor(Red: 149, Green: 165, Blue: 166)
     }
-
-    class func asbestos() -> UIColor{
-        return UIColor(R: 127, G: 140, B: 141)
+    
+    class func orange() -> UIColor {
+        return UIColor(Red: 243, Green: 156, Blue: 18)
+    }
+    
+    class func pumpkin() -> UIColor {
+        return UIColor(Red: 211, Green: 84, Blue: 0)
+    }
+    
+    class func pomegranate() -> UIColor {
+        return UIColor(Red: 192, Green: 57, Blue: 43)
+    }
+    
+    class func silver() -> UIColor {
+        return UIColor(Red: 189, Green: 195, Blue: 199)
+    }
+    
+    class func asbestos() -> UIColor {
+        return UIColor(Red: 127, Green: 140, Blue: 141)
     }
     
     //Social Colors - Values taken from https://www.lockedowndesign.com/social-media-colors/
-    class func facebook() -> UIColor{
-        return UIColor(R: 59, G: 89, B: 152)
+    class func facebook() -> UIColor {
+        return UIColor(Red: 59, Green: 89, Blue: 152)
     }
     
-    class func twitter() -> UIColor{
-        return UIColor(R: 0, G: 182, B: 241)
+    class func twitter() -> UIColor {
+        return UIColor(Red: 0, Green: 182, Blue: 241)
     }
     
-    class func googlePlus() -> UIColor{
-        return UIColor(R: 223, G: 74, B: 50)
+    class func googlePlus() -> UIColor {
+        return UIColor(Red: 223, Green: 74, Blue: 50)
     }
     
-    class func linkedIn() -> UIColor{
-        return UIColor(R: 0, G: 123, B: 182)
+    class func linkedIn() -> UIColor {
+        return UIColor(Red: 0, Green: 123, Blue: 182)
     }
     
-    class func vimeo() -> UIColor{
-        return UIColor(R: 69, G: 187, B: 255)
-    }
-
-    class func youtube() -> UIColor{
-        return UIColor(R: 179, G: 18, B: 23)
+    class func vimeo() -> UIColor {
+        return UIColor(Red: 69, Green: 187, Blue: 255)
     }
     
-    class func instagram() -> UIColor{
-        return UIColor(R: 195, G: 42, B: 163)
+    class func youtube() -> UIColor {
+        return UIColor(Red: 179, Green: 18, Blue: 23)
     }
     
-    class func pinterest() -> UIColor{
-        return UIColor(R: 203, G: 32, B: 39)
+    class func instagram() -> UIColor {
+        return UIColor(Red: 195, Green: 42, Blue: 163)
     }
     
-    class func flickr() -> UIColor{
-        return UIColor(R: 244, G: 0, B: 131)
+    class func pinterest() -> UIColor {
+        return UIColor(Red: 203, Green: 32, Blue: 39)
     }
     
-    class func yahoo() -> UIColor{
-        return UIColor(R: 67, G: 2, B: 151)
+    class func flickr() -> UIColor {
+        return UIColor(Red: 244, Green: 0, Blue: 131)
     }
     
-    class func soundCloud() -> UIColor{
-        return UIColor(R: 255, G: 85, B: 0)
+    class func yahoo() -> UIColor {
+        return UIColor(Red: 67, Green: 2, Blue: 151)
     }
     
-    class func tumblr() -> UIColor{
-        return UIColor(R: 44, G: 71, B: 98)
+    class func soundCloud() -> UIColor {
+        return UIColor(Red: 255, Green: 85, Blue: 0)
     }
     
-    class func foursquare() -> UIColor{
-        return UIColor(R: 252, G: 69, B: 117)
+    class func tumblr() -> UIColor {
+        return UIColor(Red: 44, Green: 71, Blue: 98)
     }
     
-    class func swarm() -> UIColor{
-        return UIColor(R: 255, G: 176, B: 0)
+    class func foursquare() -> UIColor {
+        return UIColor(Red: 252, Green: 69, Blue: 117)
     }
     
-    class func dribbble() -> UIColor{
-        return UIColor(R: 234, G: 76, B: 137)
+    class func swarm() -> UIColor {
+        return UIColor(Red: 255, Green: 176, Blue: 0)
     }
     
-    class func reddit() -> UIColor{
-        return UIColor(R: 255, G: 87, B: 0)
+    class func dribbble() -> UIColor {
+        return UIColor(Red: 234, Green: 76, Blue: 137)
     }
     
-    class func devianArt() -> UIColor{
-        return UIColor(R: 74, G: 93, B: 78)
+    class func reddit() -> UIColor {
+        return UIColor(Red: 255, Green: 87, Blue: 0)
     }
     
-    class func pocket() -> UIColor{
-        return UIColor(R: 238, G: 64, B: 86)
+    class func devianArt() -> UIColor {
+        return UIColor(Red: 74, Green: 93, Blue: 78)
     }
     
-    class func quora() -> UIColor{
-        return UIColor(R: 170, G: 34, B: 182)
+    class func pocket() -> UIColor {
+        return UIColor(Red: 238, Green: 64, Blue: 86)
     }
     
-    class func slideShare() -> UIColor{
-        return UIColor(R: 247, G: 146, B: 30)
+    class func quora() -> UIColor {
+        return UIColor(Red: 170, Green: 34, Blue: 182)
     }
     
-    class func px500() -> UIColor{
-        return UIColor(R: 0, G: 153, B: 229)
+    class func slideShare() -> UIColor {
+        return UIColor(Red: 247, Green: 146, Blue: 30)
     }
     
-    class func listly() -> UIColor{
-        return UIColor(R: 223, G: 109, B: 70)
+    class func px500() -> UIColor {
+        return UIColor(Red: 0, Green: 153, Blue: 229)
     }
     
-    class func vine() -> UIColor{
-        return UIColor(R: 0, G: 180, B: 137)
+    class func listly() -> UIColor {
+        return UIColor(Red: 223, Green: 109, Blue: 70)
     }
     
-    class func skype() -> UIColor{
-        return UIColor(R: 0, G: 175, B: 240)
+    class func vine() -> UIColor {
+        return UIColor(Red: 0, Green: 180, Blue: 137)
     }
     
-    class func stumbleUpon() -> UIColor{
-        return UIColor(R: 235, G: 73, B: 36)
+    class func skype() -> UIColor {
+        return UIColor(Red: 0, Green: 175, Blue: 240)
     }
     
-    class func snapchat() -> UIColor{
-        return UIColor(R: 255, G: 252, B: 0)
+    class func stumbleUpon() -> UIColor {
+        return UIColor(Red: 235, Green: 73, Blue: 36)
     }
     
-    //Hex to UIColor
-    class func colorWithHexString (hex:String) -> UIColor{
-        var hexColor:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).uppercaseString
-        
-        if (hexColor.hasPrefix("#")){
-            hexColor = (hexColor as NSString).substringFromIndex(1)
-        }
-        
-        if (hexColor.characters.count != 6){
-            return UIColor.blackColor()
-        }
-        
-        let rString = (hexColor as NSString).substringToIndex(2)
-        let gString = ((hexColor as NSString).substringFromIndex(2) as NSString).substringToIndex(2)
-        let bString = ((hexColor as NSString).substringFromIndex(4) as NSString).substringToIndex(2)
-        
-        var R:CUnsignedInt = 0, G:CUnsignedInt = 0, B:CUnsignedInt = 0;
-        NSScanner(string: rString).scanHexInt(&R)
-        NSScanner(string: gString).scanHexInt(&G)
-        NSScanner(string: bString).scanHexInt(&B)
-        
-        return UIColor(red: CGFloat(R) / 255.0, green: CGFloat(G) / 255.0, blue: CGFloat(B) / 255.0, alpha: CGFloat(1))
+    class func snapchat() -> UIColor {
+        return UIColor(Red: 255, Green: 252, Blue: 0)
     }
-    
 }
